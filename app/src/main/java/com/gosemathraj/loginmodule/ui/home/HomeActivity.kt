c99e6fb970bf61802317f16711cd1837e1782c23
NileshS <nilesh.walavalkar@wwindia.com>
2019-04-08 14:13:02 +0530

    Commit 1 : Initial Commit

---
+++ app/src/main/java/com/gosemathraj/loginmodule/ui/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.gosemathraj.loginmodule.ui.home
+
+import android.os.Bundle
+import com.gosemathraj.loginmodule.base.BaseActivity
+import com.gosemathraj.loginmodule.R
+
+class HomeActivity : BaseActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
